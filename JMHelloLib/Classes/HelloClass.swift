abc9cc7d879995183075f286655886c343020b0b
shkhan786 <shkhan786haider@gmail.com>
2017-11-19 19:13:25 +0530

    ad

---
+++ JMHelloLib/Classes/HelloClass.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  JMHelloLib
+//
+//  Created by S Haider Khan on 19/11/17.
+//
+
+import Foundation
+public class HelloClass : NSObject{
+    
+    public static func hello(){
+        print("Hello this is second lib for cocoapod i am very ecxited")
+    }
+}
